49d5b577c84906a96a3d67994d9b7c42609f1fad
unknown <Pruchkin-Maksim@yandex.ru>
2022-06-21 00:15:41 +0300

    завершил

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 import * as data from './data.js';
-import './render-thumbnails.js';
+import {createThumbnail} from './render-thumbnails.js';
 
 const COUNT_OBJECT = 25;
 
 const arrayData = data.createData(COUNT_OBJECT);
+
+const thumbnail = createThumbnail(arrayData);
+const pictures = document.querySelector('.pictures');
+pictures.append(thumbnail);
--- js/render-thumbnails.js
+++ js/render-thumbnails.js
@@ -1,16 +1,15 @@
-const templateFragment = document.querySelector('#picture').content;
-const templateThumbnail = templateFragment.querySelector('a');
-
-const fragment = document.createDocumentFragment();
-
-for (let i=0; i < 25; i++){
-  const element = templateThumbnail.cloneNode(true);
-  fragment.appendChild(element);
+function createThumbnail (data) {
+  const templateFragment = document.querySelector('#picture').content;
+  const templateThumbnail = templateFragment.querySelector('a');
+  const fragment = document.createDocumentFragment();
+  for (let i=0; i < data.length; i++){
+    const element = templateThumbnail.cloneNode(true);
+    element.children[0].src = data[i].url;
+    element.children[1].children[1].innerText = data[i].likes;
+    element.children[1].children[0].innerText = data[i].comments.length;
+    fragment.appendChild(element);
+  }
+  return fragment;
 }
-console.log(fragment);
-// На основе временных данных для разработки и шаблона #picture создайте DOM-элементы, соответствующие фотографиям, и заполните их данными:
 
-// Адрес изображения url подставьте как атрибут src изображения.
-// Количество лайков likes выведите в блок .picture__likes.
-// Количество комментариев comments выведите в блок .picture__comments.
-// Отрисуйте сгенерированные DOM-элементы в блок .pictures. Для вставки элементов используйте DocumentFragment.
+export {createThumbnail};
