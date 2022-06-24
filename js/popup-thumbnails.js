ddea795bbf0694163caafc15da0fdb1c77fb2db8
Apocalipso <pruchkin-maksim@yandex.ru>
2022-06-24 14:48:51 +0300

    почти доделал открытие модального окна

--- js/popup-thumbnails.js
+++ js/popup-thumbnails.js
@@ -1,9 +1,25 @@
-//отрисовка окна с полноразмерным изображением
-//функция отрисовки данных в попап
 function showPopupData(data, evt){
   evt.preventDefault();
-  const imgUpload = document.querySelector('.big-picture');
-  imgUpload.classList.remove('hidden');
+  const bigPicture = document.querySelector('.big-picture');
+  const bigPictureImg = bigPicture.querySelector('.big-picture__img img');
+  const likes = bigPicture.querySelector('.likes-count');
+  const socialCaption = bigPicture.querySelector('.social__caption');
+  const commentsCount = bigPicture.querySelector('.comments-count');
+  const socCommentsCount = bigPicture.querySelector('.social__comment-count');
+  const commentsLoader = bigPicture.querySelector('.comments-loader');
+
+  const socialComments = bigPicture.querySelector('.social__comments');
+  socialComments.remove();
+
+  commentsLoader.classList.add('hidden');
+  socCommentsCount.classList.add('hidden');
+  commentsCount.innerText = data.comments.length;
+  //Список комментариев под фотографией: комментарии должны вставляться в блок .social__comments. Разметка каждого комментария должна выглядеть так:
+  socialCaption.innerText = data.description;
+  likes.innerText = data.likes;
+  bigPictureImg.alt = data.description;
+  bigPictureImg.src = data.url;
+  bigPicture.classList.remove('hidden');
   return;
 }
 
