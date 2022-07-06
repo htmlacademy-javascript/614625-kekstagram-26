3756585dd40e863672657b36140413e090e5484d
Apocalipso <pruchkin-maksim@yandex.ru>
2022-07-06 15:24:40 +0300

    добавил в код пристин

--- js/main.js
+++ js/main.js
@@ -11,6 +11,8 @@ const thumbnail = createThumbnails(arrayData);
 const pictures = document.querySelector('.pictures');
 pictures.append(thumbnail);
 
+const imgForm = document.querySelector('.img-upload__form');
+const pristine = new Pristine(imgForm);
 const buttonClose = document.querySelector('.big-picture__cancel');
 const body = document.querySelector('body');
 const buttonUpload = document.querySelector('.img-upload__input');
@@ -40,20 +42,29 @@ buttonClose.addEventListener('click', () => {
 });
 
 buttonUpload.addEventListener('change', (evt) => {
-  console.log(evt.target.files[0]);
+
   imgUpload.classList.remove('hidden');
   const [file] = buttonUpload.files;
+
   imgUpload.querySelector('img').src = URL.createObjectURL(file);
 });
 
+imgForm.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  const isValid = pristine.validate();
+  if (isValid) {
+    console.log('все норм');
+  } else {
+    console.log('не норм');
+  }
+})
 // var control = document.getElementById("your-files");
 // control.addEventListener("change", function(event) {
 //   // Когда происходит изменение элементов управления, значит появились новые файлы
-//   var i = 0,
 //     files = control.files,
 //     len = files.length;
 
-//   for (; i < len; i++) {
+//   for (var i = 0; i < len; i++) {
 //     console.log("Filename: " + files[i].name);
 //     console.log("Type: " + files[i].type);
 //     console.log("Size: " + files[i].size + " bytes");
