a3c1ddbe69d1489aaea6ab3f956c2ed873494a94
unknown <Pruchkin-Maksim@yandex.ru>
2022-07-12 03:02:22 +0300

    написал функцию валидации хештега

--- js/main.js
+++ js/main.js
@@ -38,10 +38,6 @@ const onCloseEscKeydown = (evt) => {
 
 const textHashtags = document.querySelector('.text__hashtags');
 
-textHashtags.addEventListener('focus', () => {
-  onCloseEscKeydown.stopPropagation();
-});
-
 pictures.onclick = function (evt) {
   if (evt.target.nodeName === 'IMG') {
     body.classList.add('modal-open');
@@ -66,7 +62,7 @@ buttonClose.addEventListener('click', () => {
 buttonUpload.addEventListener('change', () => {
   body.classList.add('modal-open');
   imgUpload.classList.remove('hidden');
-  const [file] = buttonUpload.files;
+  const file = buttonUpload.files[0];
   imgUpload.querySelector('img').src = URL.createObjectURL(file);
   document.addEventListener('keydown', onCloseEscKeydown);
 });
@@ -74,6 +70,7 @@ buttonUpload.addEventListener('change', () => {
 imgForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
   const isValid = pristine.validate();
+  validateHashTeg(textHashtags.value);
   //валидация хеш тега
   if (isValid) {
     console.log('все норм');
@@ -81,3 +78,29 @@ imgForm.addEventListener('submit', (evt) => {
     console.log('не норм');
   }
 });
+
+function validateHashTeg(hashteg){
+  hashteg = hashteg.split(' ');
+  if(hashteg.length>5){
+    return 'нельзя указывать больше 5 хештегов';
+  }
+  const allowCharacters = /(([a-zA-Z].*\d)|(\d.*[a-zA-Z]))/;
+  // строка после решётки должна состоять из букв и чисел и не может содержать пробелы, спецсимволы (#, @, $ и т. п.), символы пунктуации (тире, дефис, запятая и т. п.), эмодзи и т. д.;
+  for (let i = 0; i<hashteg.length;i++){
+    if(hashteg[i][0] !== '#'){
+      return 'хэш-тег начинается с символа #';
+    }
+    if (allowCharacters.test(hashteg[i]) === false){
+      return 'строка после решётки должна состоять из букв';
+    }
+    if(hashteg[i].length>20){
+      return 'максимальная длина одного хэш-тега 20 символов';
+    }
+    if(hashteg[i]==='#'){
+      return 'хеш-тег не может состоять только из одной решётки';
+    }
+    if(hashteg.indexOf(hashteg[i]) !== -1){
+      return 'один и тот же хэш-тег не может быть использован дважды';
+    }
+  }
+}
