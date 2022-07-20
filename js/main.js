86f81a7b58247f9aebaae3e8079bc201b724ed49
unknown <Pruchkin-Maksim@yandex.ru>
2022-07-21 00:17:33 +0300

    приступил к выполнению

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-import * as data from './data.js';
 import {createThumbnails} from './render-thumbnails.js';
 import {showPopupData, closePopupData} from './popup-thumbnail.js';
 import {isEscapeKey} from './util.js';
@@ -7,15 +6,28 @@ import './edit-scaleimg.js';
 import './effects-img.js';
 
 const COUNT_OBJECT = 25;
+const body = document.querySelector('body');
 
-const arrayData = data.createData(COUNT_OBJECT);
+fetch('https://26.javascript.pages.academy/kekstagram/data')
+  .then((response) => response.json())
+  .then((data)=> {
+    const arrayData = data.slice(0, COUNT_OBJECT);
 
-const thumbnail = createThumbnails(arrayData);
-const pictures = document.querySelector('.pictures');
-pictures.append(thumbnail);
+    const thumbnail = createThumbnails(arrayData);
+    const pictures = document.querySelector('.pictures');
+    pictures.append(thumbnail);
+
+    pictures.onclick = function (evt) {
+      if (evt.target.nodeName === 'IMG') {
+        body.classList.add('modal-open');
+        showPopupData(arrayData[evt.target.offsetParent.dataset.id-1],evt);
+        document.addEventListener('keydown', onPopupEscKeydown);
+      }
+    };
+  });
 
 const buttonClose = document.querySelector('.big-picture__cancel');
-const body = document.querySelector('body');
+
 
 function onPopupEscKeydown(evt){
   if (isEscapeKey(evt)) {
@@ -25,14 +37,6 @@ function onPopupEscKeydown(evt){
     document.removeEventListener('keydown', onPopupEscKeydown);
   }
 }
-
-pictures.onclick = function (evt) {
-  if (evt.target.nodeName === 'IMG') {
-    body.classList.add('modal-open');
-    showPopupData(arrayData[evt.target.offsetParent.dataset.id-1],evt);
-    document.addEventListener('keydown', onPopupEscKeydown);
-  }
-};
 
 buttonClose.addEventListener('click', () => {
   body.classList.remove('modal-open');
